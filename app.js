8bacce828b5c613ecd8ef224d7af428924e1a973
ra7ali <rahali.fahd@gmail.com>
2020-01-30 22:56:01 +0100

    ajout des styles avec javascript

--- app.js
+++ app.js
@@ -7,4 +7,43 @@ function popup() {
 
 }
 
+
+document.querySelector('button')
+    .addEventListener('click', function() {
+        document.body.style.backgroundColor = "magenta"
+        document.getElementsByTagName('h1')[0].style.color = 'green';
+        document.getElementsByTagName('h1')[0].style.fontFamily = 'Comic Sans';
+
+        document.getElementsByTagName('h4')[0].style.color = 'green';
+        document.getElementsByTagName('h4')[0].style.fontFamily = 'Comic Sans';
+
+
+        let ps = document.getElementsByTagName('p');
+        let titlesH2 = document.getElementsByTagName('h2');
+        let titlesH3 = document.getElementsByTagName('h3');
+
+
+
+        let i;
+        for (i = 0; i < ps.length; i++) {
+
+            ps[i].style.color = 'blue';
+            ps[i].style.fontFamily = 'courier';
+
+        }
+
+        for (i = 0; i < titlesH2.length; i++) {
+
+            titlesH2[i].style.color = 'green';
+            titlesH2[i].style.fontFamily = 'Comic Sans';
+        }
+
+        for (i = 0; i < titlesH3.length; i++) {
+
+            titlesH3[i].style.color = 'green';
+            titlesH3[i].style.fontFamily = 'Comic Sans';
+
+        }
+    })
+
 setTimeout(function() { popup() }, 10000);
